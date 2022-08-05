c96397aed2b65c084edfe7b9b653a3655c70a1ff
RGLLNZ <191186@unamba.edu.pe>
2022-08-04 23:59:03 -0500

    Inicializando el proyecto

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
-#ifdef __APPLE__
-# include <OpenGL/gl.h>
-# include <OpenGL/glu.h>
-# include <GLUT/glut.h>
-#else
-# include <GL/gl.h>
-# include <GL/glu.h>
-# include <GL/glut.h>
-#endif
+#include <iostream>
+#include <GL/glut.h> 
+#include <windows.h>
+#include <time.h>
 
+#define XLIM 1280
+#define YLIM 640
 
-
+using namespace std;
+void pantalla(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
 int main (int argc, char **argv) {
-	glutInit (&argc, argv);
-	initialize();
+	glutInit(&argc, argv);
+	glutInitWindowSize(XLIM,YLIM);
+	glutInitWindowPosition(10,10);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutCreateWindow("Game Of Life");
+	glutDisplayFunc(pantalla);
 	glutMainLoop();
 	return 0;
-	
-	cout<<"Hola mundo";
 }
